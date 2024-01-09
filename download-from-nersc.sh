b7ca20fc4bac80748b695e91bf891be9e88b87fd
ashodkh <ask126@pitt.edu>
2024-01-09 10:52:30 -0500

    updated to latest run that uses fastspecfit v3.1

--- download-from-nersc.sh
+++ download-from-nersc.sh
@@ -4,7 +4,7 @@ lines=("OII_DOUBLET" "HGAMMA" "HBETA" "OIII_4959" "OIII_5007" "NII_6548" "HALPHA
 #lines=("OII_DOUBLET")
 for l in ${lines[*]}; do
     for i in {0..10}; do
-        scp  -i ~/.ssh/nersc ashodkh@perlmutter-p1.nersc.gov:/pscratch/sd/a/ashodkh/fluxes_from_spectra/raw_masked/sv3_fluxes_raw_masked${i}"_selection4_"${l}"_bins13.txt.npz" ~/Desktop/research/git_repos/emission-lines/data/fluxes/
+        scp  -i ~/.ssh/nersc ashodkh@perlmutter-p1.nersc.gov:/pscratch/sd/a/ashodkh/fluxes_from_spectra/raw_masked/sv3_fluxes_raw_masked${i}"_selection5_"${l}"_bins13.txt.npz" ~/Desktop/research/git_repos/emission-lines/data/fluxes/
     done
 done
 
